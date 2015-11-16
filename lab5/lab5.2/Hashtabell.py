13aed121d28c394e08b55a8f4a4bd6775d46483f
Robert Åberg <robertab@kth.se>
2015-11-16 16:54:16 +0100

    Laboration 5 - Färdig

--- lab5/lab5.2/Hashtabell.py
+++ lab5/lab5.2/Hashtabell.py
@@ -5,34 +5,60 @@ Uppgift: Lab5.2
 """
 
 
-"""Hjälpklass till HashTabell"""
+"""Hjälpklass till Hashtabell"""
 class HashNode:
     def __init__(self, key, value):
-        self.key = key
-        self.value = value
+        self.key = key # låtnamn efter hashfuntion
+        self.value = value # låtobjektet
+
+    def getKey(self):
+        return self.key
+
+    def getValue(self):
+        return self.value
 
 
+class Hashtabell:
+    def __init__(self, size= 20000000):
+        self.size =  size
+        self.hashList = [None]*self.size
 
+    def put(self, key, value): 
+        hashIndex = hashfunction(key, self.size) # Sksapar ett index för objektet
 
-class HashTabell:
-    def __init__(self, alist):
-        self.size = len(alist) * 2
-        
+        while self.hashList[hashIndex] != None:
+            hashIndex = rehash(hashIndex, self.size)
 
-    def put(self, key, value):
-        pass
+        self.hashList[hashIndex] = HashNode(key, value) # Lägger till objektet i hashlistan på plats hashIndex
+
 
     def get(self, key):
-        pass
+        hashIndex = hashfunction(key, self.size)
 
-    def __str__(self):
-        pass
+        try: 
+            while self.hashList[hashIndex] != None:
 
-    def hasKey(self, key):
-        pass
+                if self.hashList[hashIndex].getKey() == key:
+                    return self.hashList[hashIndex].getValue()
+                else:
+                    hashIndex = rehash(hashIndex, self.size)
 
-    def getSize(self):
-        return self.size
+        except KeyError:
+            return False
+ 
+def hashfunction(key, size):
+    result = 0
+    for letter in key:
+        result = result*32 + ord(letter)
+
+    return result%size
+
+def rehash(prevHash, size):
+    return (prevHash+1)%size
+
+
+    
+
         
         
         
